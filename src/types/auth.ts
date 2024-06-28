43a5660404e31940dbc241c8b480371fa0e807f3
nathakusuma <nathakusuma07@gmail.com>
2024-06-29 03:37:03 +0700

    ``feat(auth)``: add auth backend request & response

---
+++ src/types/auth.ts
@@ -0,0 +1,16 @@
+export interface RegisterRequest {
+    fullName: string
+    email: string
+    phoneNumber: string
+    password: string
+}
+
+export interface LoginRequest {
+    email: string
+    password: string
+}
+
+export interface LoginResponse {
+    token: string
+    fullName: string
+}
