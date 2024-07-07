a62a941c509428df69f595d6ad27f761069f47b5
nathakusuma <nathakusuma07@gmail.com>
2024-07-07 23:00:50 +0700

    ``style(navbar)``: make all links active. On hover, change the text color.

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,39 +1,8 @@
-import React, { useEffect, useState } from "react";
 import Logo from "../../assets/img/logo.png";
 import {useAuth} from "../../hooks/useAuth.ts";
+import "./navbar.css";
 
 const Navbar: React.FC = () => {
-    const [activeSection, setActiveSection] = useState<string>('home');
-
-    useEffect(() => {
-        const sections = document.querySelectorAll('section');
-        const options = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.7
-        };
-
-        const callback: IntersectionObserverCallback = (entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    setActiveSection(entry.target.id);
-                }
-            });
-        };
-
-        const observer = new IntersectionObserver(callback, options);
-
-        sections.forEach(section => {
-            observer.observe(section);
-        });
-
-        return () => {
-            sections.forEach(section => {
-                observer.unobserve(section);
-            });
-        };
-    }, []);
-
     const isAuthenticated = useAuth();
 
     return (
@@ -51,23 +20,23 @@ const Navbar: React.FC = () => {
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className={`nav-link ${activeSection === 'home' ? 'active' : ''}`} aria-current="page" href="/#home">Home</a>
+                            <a className="nav-link active" aria-current="page" href="/#home">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${activeSection === 'services' ? 'active' : ''}`} href="/#services">Services</a>
+                            <a className="nav-link active" href="/#services">Services</a>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${activeSection === 'branches' ? 'active' : ''}`} href="/#branches">Branches</a>
+                            <a className="nav-link active" href="/#branches">Branches</a>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${activeSection === 'reviews' ? 'active' : ''}`} href="/reviews">Reviews</a>
+                            <a className="nav-link active" href="/reviews">Reviews</a>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${activeSection === 'contact' ? 'active' : ''}`} href="/#contact">Contact</a>
+                            <a className="nav-link active" href="/#contact">Contact</a>
                         </li>
                         {isAuthenticated ? (
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">My Account</a>
+                                <a className="nav-link active dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">My Account</a>
                                 <ul className="dropdown-menu">
                                     <li><a className="dropdown-item" href="/dashboard">Dashboard</a></li>
                                     <li><hr className="dropdown-divider"></hr></li>
