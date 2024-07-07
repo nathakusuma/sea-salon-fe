f4eb4bec900cd0a2b01afdbdbaad100ea768e993
nathakusuma <nathakusuma07@gmail.com>
2024-07-07 21:45:59 +0700

    ``feat``: add interceptor to catch 401 error from backend

--- src/api/coreApi.ts
+++ src/api/coreApi.ts
@@ -1,7 +1,24 @@
 import axios, {AxiosInstance} from "axios";
+import { createBrowserHistory } from "history";
 
 const axiosInstance : AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
+const history = createBrowserHistory()
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 401) {
+            window.localStorage.removeItem("token");
+            window.localStorage.removeItem("fullName");
+            window.localStorage.removeItem("isAdmin");
+            history.push("/login")
+            window.location.reload();
+        }
+        return Promise.reject(error);
+    }
+);
 
 export { axiosInstance }
